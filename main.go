7133dd3ba2ba9c5a7dfa9a493eccbd440831250a
MarwanRadwan7 <marwan.swe@outlook.com>
2024-10-09 15:02:07 +0300

    feat: Add docker client to Task to manage running and stopping containers

--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/MarwanRadwan7/cube/task"
+	"github.com/docker/docker/client"
+)
+
+func createContainer() (*task.Docker, *task.DockerResult) {
+	c := task.Config{
+		Name:  "test-container-1",
+		Image: "hello-world",
+		// Env: []string{
+		// 	"POSTGRES_USER=cube",
+		// 	"POSTGRES_PASSWORD=secret",
+		// },
+	}
+	dc, _ := client.NewClientWithOpts(client.FromEnv)
+	d := task.Docker{
+		Client: dc,
+		Config: c,
+	}
+	result := d.Run()
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil, nil
+	}
+	fmt.Printf(
+		"Container %s is running with config %v\n", result.ContainerId, c)
+	return &d, &result
+}
+
+func stopContainer(d *task.Docker, id string) *task.DockerResult {
+	result := d.Stop(id)
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil
+	}
+	fmt.Printf(
+		"Container %s has been stopped and removed\n", result.ContainerId)
+	return &result
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Printf("create a test container\n")
+	dockerTask, createResult := createContainer()
+	if createResult.Error != nil {
+		fmt.Printf("%v", createResult.Error)
+		os.Exit(1)
+	}
+	time.Sleep(time.Second * 10)
+	fmt.Printf("stopping container %s\n", createResult.ContainerId)
+	_ = stopContainer(dockerTask, createResult.ContainerId)
 }
