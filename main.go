9ece25735082926e12274346fe5145033316a75e
MarwanRadwan7 <marwan.swe@outlook.com>
2024-10-10 12:16:47 +0300

    Test the worker and run simple container

--- main.go
+++ main.go
@@ -2,56 +2,47 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/MarwanRadwan7/cube/task"
-	"github.com/docker/docker/client"
+	"github.com/MarwanRadwan7/cube/worker"
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
 )
 
-func createContainer() (*task.Docker, *task.DockerResult) {
-	c := task.Config{
-		Name:  "test-container-1",
-		Image: "hello-world",
-		// Env: []string{
-		// 	"POSTGRES_USER=cube",
-		// 	"POSTGRES_PASSWORD=secret",
-		// },
-	}
-	dc, _ := client.NewClientWithOpts(client.FromEnv)
-	d := task.Docker{
-		Client: dc,
-		Config: c,
-	}
-	result := d.Run()
-	if result.Error != nil {
-		fmt.Printf("%v\n", result.Error)
-		return nil, nil
-	}
-	fmt.Printf(
-		"Container %s is running with config %v\n", result.ContainerId, c)
-	return &d, &result
-}
-
-func stopContainer(d *task.Docker, id string) *task.DockerResult {
-	result := d.Stop(id)
-	if result.Error != nil {
-		fmt.Printf("%v\n", result.Error)
-		return nil
-	}
-	fmt.Printf(
-		"Container %s has been stopped and removed\n", result.ContainerId)
-	return &result
-}
+// FIXME: Fix naming the containers
 
 func main() {
-	fmt.Printf("create a test container\n")
-	dockerTask, createResult := createContainer()
-	if createResult.Error != nil {
-		fmt.Printf("%v", createResult.Error)
-		os.Exit(1)
+	db := make(map[uuid.UUID]*task.Task)
+	w := worker.Worker{
+		Queue: *queue.New(),
+		Db:    db,
 	}
-	time.Sleep(time.Second * 10)
-	fmt.Printf("stopping container %s\n", createResult.ContainerId)
-	_ = stopContainer(dockerTask, createResult.ContainerId)
+	t := task.Task{
+		ID:    uuid.New(),
+		Name:  "test-container-1",
+		State: task.Scheduled,
+		Image: "strm/helloworld-http",
+	}
+	// first time the worker will see the task
+	fmt.Println("starting task")
+	w.AddTask(t)
+	result := w.RunTask()
+	if result.Error != nil {
+		panic(result.Error)
+	}
+
+	t.ContainerID = result.ContainerId
+	fmt.Printf("task %s is running in container %s\n", t.ID, t.ContainerID)
+
+	fmt.Println("Sleepy time")
+	time.Sleep(time.Second * 4)
+
+	fmt.Printf("stopping task %s\n", t.ID)
+	t.State = task.Completed
+	w.AddTask(t)
+	result = w.RunTask()
+	if result.Error != nil {
+		panic(result.Error)
+	}
 }
