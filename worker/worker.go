e73a4327548b86247a27ac4d7699a4f71f8b2666
MarwanRadwan7 <marwan.swe@outlook.com>
2024-10-10 12:16:27 +0300

    feat: Add the logic of the Worker

--- worker/worker.go
+++ worker/worker.go
@@ -2,6 +2,8 @@ package worker
 
 import (
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/MarwanRadwan7/cube/task"
 	"github.com/golang-collections/collections/queue"
@@ -21,16 +23,93 @@ func (w *Worker) CollectStats() {
 }
 
 // RunTask responsible for identifying the task’s current state then either starting or stopping a task based on the state.
-func (w *Worker) RunTask() {
-	fmt.Println("I will start or stop the task based on its state")
+func (w *Worker) RunTask() task.DockerResult {
+	t := w.Queue.Dequeue()
+	if t == nil {
+		log.Println("No tasks to run in the queue")
+		return task.DockerResult{Error: nil}
+	}
+
+	taskQueued, ok := t.(task.Task)
+	if !ok {
+		log.Println("Error dequeued item is not of type task.Task")
+		return task.DockerResult{Error: fmt.Errorf("dequeued item is not of type task.Task")}
+	}
+
+	taskPersisted := w.Db[taskQueued.ID]
+	// First Time
+	if taskPersisted == nil {
+		taskPersisted = &taskQueued
+		w.Db[taskQueued.ID] = &taskQueued
+	}
+
+	// Nth Time
+	if !task.ValidStateTransition(taskPersisted.State, taskQueued.State) {
+		err := fmt.Errorf("invalid transition from %v to %v for task %v", taskPersisted.State, taskQueued.State, taskQueued.ID)
+		return task.DockerResult{Error: err}
+	}
+	var result task.DockerResult
+	switch taskQueued.State {
+	case task.Scheduled:
+		result = w.StartTask(taskQueued)
+	case task.Completed:
+		result = w.StopTask(taskQueued)
+	default:
+		log.Printf("Bad behavior in state transition for task %v", taskQueued.ID)
+		result.Error = fmt.Errorf("bad behavior in state transition for task %v", taskQueued.ID)
+	}
+
+	return result
 }
 
 // StartTask starts a task for the worker.
-func (w *Worker) StartTask() {
-	fmt.Println("I will start a task")
+func (w *Worker) StartTask(t task.Task) task.DockerResult {
+	t.StartTime = time.Now().UTC()
+	config := task.NewConfig(&t)
+	d, err := task.NewDocker(config)
+	if err != nil {
+		log.Printf("Error creating a docker demon for task %s: %v\n", t.ID, err)
+		return task.DockerResult{Error: err}
+	}
+	result := d.Run()
+	if result.Error != nil {
+		log.Printf("Error running task %s: %v\n", t.ID, err)
+		t.State = task.Failed
+		w.Db[t.ID] = &t
+		return result
+	}
+
+	t.ContainerID = result.ContainerId
+	t.State = task.Running
+	w.Db[t.ID] = &t
+
+	return result
 }
 
 // StopTask stops a task for the worker.
-func (w *Worker) StopTask() {
-	fmt.Println("I will stop a task")
+func (w *Worker) StopTask(t task.Task) task.DockerResult {
+	config := task.NewConfig(&t)
+	d, err := task.NewDocker(config)
+	if err != nil {
+		log.Printf("Error creating a docker demon for task %s: %v\n", t.ID, err)
+		return task.DockerResult{Error: err}
+	}
+
+	result := d.Stop(t.ContainerID)
+	if result.Error != nil {
+		log.Printf("Error stooping container %s: %v\n", t.ContainerID, result.Error)
+		return task.DockerResult{Error: err}
+	}
+
+	t.FinishTime = time.Now().UTC()
+	t.State = task.Completed
+	w.Db[t.ID] = &t
+	log.Printf("Stopped and removed container %s for task %s\n", t.ContainerID, t.ID)
+
+	return result
+}
+
+// AddTask adds a new task to the worker's queue.
+func (w *Worker) AddTask(t task.Task) {
+	w.Queue.Enqueue(t)
 }
