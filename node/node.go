fa8e6874f8efb23f9b487a8dc748347991b7bd20
MarwanRadwan7 <marwan.swe@outlook.com>
2024-10-26 12:14:38 +0300

    feat: Add getting stats of node

--- node/node.go
+++ node/node.go
@@ -1,14 +1,29 @@
 package node
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/MarwanRadwan7/cube/stats"
+	"github.com/MarwanRadwan7/cube/utils"
+)
+
+// FIXME: Differ between Ip and API
+
 // Node represents the physical aspect of the Worker.
 type Node struct {
 	Name            string
 	Ip              string
-	Cores           int
-	Memory          int
-	MemoryAllocated int
-	Disk            int
-	DiskAllocated   int
+	Cores           int64
+	Memory          int64
+	MemoryAllocated int64
+	Disk            int64
+	DiskAllocated   int64
+	Stats           stats.Stats
 	Role            string
 	TaskCount       int
 }
@@ -19,4 +34,43 @@ func NewNode(worker string, api string, role string) *Node {
 		Ip:   api,
 		Role: role,
 	}
+}
+
+func (n *Node) GetStats() (*stats.Stats, error) {
+	var resp *http.Response
+	var err error
+
+	url := fmt.Sprintf("%s/stats", n.Ip)
+	resp, err = utils.HTTPWithRetry(http.Get, url)
+	if err != nil {
+		msg := fmt.Sprintf("Unable to connect to %v. Permanent failure.\n", n.Ip)
+		log.Println(msg)
+		return nil, errors.New(msg)
+	}
+
+	if resp.StatusCode != 200 {
+		msg := fmt.Sprintf("Error retrieving stats from %v: %v", n.Ip, err)
+		log.Println(msg)
+		return nil, errors.New(msg)
+	}
+
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	var stats stats.Stats
+	err = json.Unmarshal(body, &stats)
+	if err != nil {
+		msg := fmt.Sprintf("error decoding message while getting stats for node %s", n.Name)
+		log.Println(msg)
+		return nil, errors.New(msg)
+	}
+
+	if stats.MemStats == nil || stats.DiskStats == nil {
+		return nil, fmt.Errorf("error getting stats from node %s", n.Name)
+	}
+
+	n.Memory = int64(stats.MemTotalKb())
+	n.Disk = int64(stats.DiskTotal())
+	n.Stats = stats
+
+	return &n.Stats, nil
 }
