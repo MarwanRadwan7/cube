57385739ab4103508f50ebce5931bb892e13416c
MarwanRadwan7 <marwan.swe@outlook.com>
2024-11-09 16:57:30 +0200

    feat: Add worker's CLI

--- go.mod
+++ go.mod
@@ -20,14 +20,18 @@ require (
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/spf13/cobra v1.8.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/net v0.0.0-20210929193557-e81a3d93ecf6 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 	google.golang.org/genproto v0.0.0-20201110150050-8816d57aaa9a // indirect
 	google.golang.org/grpc v1.41.0 // indirect
 	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,69 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-
-	"github.com/MarwanRadwan7/cube/manager"
-	"github.com/MarwanRadwan7/cube/worker"
-	"github.com/joho/godotenv"
-)
+import "github.com/MarwanRadwan7/cube/cmd"
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	whost := os.Getenv("CUBE_WORKER_HOST")
-	wport, _ := strconv.Atoi(os.Getenv("CUBE_WORKER_PORT"))
-
-	mhost := os.Getenv("CUBE_MANAGER_HOST")
-	mport, _ := strconv.Atoi(os.Getenv("CUBE_MANAGER_PORT"))
-
-	fmt.Println("Starting Cube Worker")
-
-	// Initially, Start 3 Workers
-	// w1 := worker.New("worker-1", "memory")
-	// w2 := worker.New("worker-2", "memory")
-	// w3 := worker.New("worker-3", "memory")
-	w1 := worker.New("worker-1", "persistent")
-	w2 := worker.New("worker-2", "persistent")
-	w3 := worker.New("worker-3", "persistent")
-	wapi1 := worker.Api{Address: whost, Port: wport, Worker: w1}
-	wapi2 := worker.Api{Address: whost, Port: wport + 1, Worker: w2}
-	wapi3 := worker.Api{Address: whost, Port: wport + 2, Worker: w3}
-
-	// Worker 1
-	go w1.RunTasks()
-	go w1.UpdateTasks()
-	go wapi1.Start()
-
-	// Worker 2
-	go w2.RunTasks()
-	go w2.UpdateTasks()
-	go wapi2.Start()
-
-	// Worker 3
-	go w3.RunTasks()
-	go w3.UpdateTasks()
-	go wapi3.Start()
-
-	fmt.Println("Starting Cube Manager")
-	workers := []string{
-		fmt.Sprintf("%s:%d", whost, wport),
-		fmt.Sprintf("%s:%d", whost, wport+1),
-		fmt.Sprintf("%s:%d", whost, wport+2),
-	}
-
-	//m := manager.New(workers, "roundrobin")
-	m := manager.New(workers, "epvm", "memory")
-	mapi := manager.Api{Address: mhost, Port: mport, Manager: m}
-	go m.ProcessTasks()
-	go m.UpdateTasks()
-	go m.DoHealthChecks()
-
-	mapi.Start()
+	cmd.Execute()
 }
